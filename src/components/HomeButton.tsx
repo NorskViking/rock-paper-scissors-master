bad041bae5c9cc5325e1db01031a22d9f4ff0e0e
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-05-29 15:20:47 +0200

    Update HomeButton.tsx
    HomeButton used "Link" from react-router-dom, changed to html 'a' tag.

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom";
-
 const HomeButton = () => {
     return (
-        <nav className="flex absolute items-center justify-center right-0 left-0 bottom-20 sm:bottom-10">
+        <a className="flex absolute items-center justify-center right-0 left-0 bottom-20 sm:bottom-10" href="https://norskviking.github.io/portfolio" target="_blank">
             <button className="flex items-center px-3 py-1 border-2 rounded text-lg text-white hover:text-red-500">
-                <Link to="https://norskviking.github.io/portfolio">HOMEPAGE</Link>
+                HOMEPAGE
             </button>
-        </nav>
+        </a>
     )
 }
 
