c2e9b07835b0c4d5b5e8a2197c9d0cb61ce4fc21
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-05-30 10:20:13 +0200

    Update HomeButton.tsx

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,6 +1,6 @@
 const HomeButton = () => {
     return (
-        <a className="flex absolute items-center justify-center right-0 left-0 bottom-20 sm:bottom-10" href="https://norskviking.github.io/portfolio" target="_blank">
+        <a className="flex absolute items-center justify-center right-0 left-0 bottom-20 sm:bottom-10" href="https://norskviking.github.io/portfolio/" target="_blank">
             <button className="flex items-center px-3 py-1 border-2 rounded text-lg text-white hover:text-red-500">
                 HOMEPAGE
             </button>
